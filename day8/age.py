ce8412faa0dd58f1d8bb50bd64afa89ae6b54050
moynak04 <sinhamoynak@gmail.com>
2026-04-04 22:16:38 +0530

    age discriminator

---
+++ day8/age.py
@@ -0,0 +1,14 @@
+# Age Discriminator Program
+
+age = int(input("Enter your age: "))
+
+if age < 0:
+    print("Invalid age entered!")
+elif age <= 12:
+    print("You are a Child")
+elif age <= 19:
+    print("You are a Teenager")
+elif age <= 59:
+    print("You are an Adult")
+else:
+    print("You are a Senior Citizen")
